30cd89bfe4e92d32ecf1fc2c137de576b0d75937
Andy Chen <andychen0310@gmail.com>
2015-06-29 09:46:18 +0800

    chart(developing)

--- application/assets/js/project_list9.js
+++ application/assets/js/project_list9.js
@@ -846,6 +846,66 @@ function show_more_content(row)
 		row.style.whiteSpace = "normal";
 	}
 }
+
+/**
+圖表瀏覽功能
+*/
+function view_chart()
+{
+	var server_ip_address = document.getElementById("server_ip_address").value;
+	//var file_path = 'http://'+server_ip_address+'/project_management/application/assets/vp_meeting/'+preview_file+'.pdf';
+	var myChart = echarts.init(document.getElementById('view_chart'));
+    myChart.setOption({
+        tooltip : {  //滑鼠移到bar上面所要呈現的資訊
+            trigger: 'axis'
+        },
+        legend: {  //圖例
+            data:['蒸发量','降水量']
+        },
+        toolbox: {
+            show : true,  //顯示(true)或隱藏(false)工具箱
+            feature : {  
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                magicType : {show: true, type: ['line', 'bar']},
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        calculable : true,  //是否讓使用者可以拖曳資料重新計算圖表
+        xAxis : [
+            {
+                type : 'category',
+                data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                splitArea : {show : true}  //Y軸row上有顏色區分標示
+            }
+        ],
+        series : [  //資料值
+            {
+                name:'蒸发量',
+                type:'bar',
+                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+            },
+            {
+                name:'降水量',
+                type:'bar',
+                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+            }
+        ]
+    });
+	document.getElementById("view_chart_block").style.display="block";
+	document.getElementById("view_chart_background_mask").style.display="block";
+}
+function close_view_chart(trigger_element_id)
+{
+	document.getElementById("view_chart_block").style.display="none";
+	document.getElementById("view_chart_background_mask").style.display="none";
+}
 /**
 開啟pdf檔案預覽功能
 */
